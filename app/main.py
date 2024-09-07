728a22c50846a1e1cafd30dae9208bacd9d1241d
DiazJSC <diazjuanse22@gmail.com>
2024-09-07 17:14:58 -0500

    Se inicia con la implementacion general del funcionamiento del sistema en la clase main.py, obteniendo como resultado la opción de generar reportes. Se hacen actualizaciones en la clase Reportes para que se utilizar en la anterior clase

---
+++ app/main.py
@@ -0,0 +1,35 @@
+#from models.pacientes import Paciente
+from models.reportes import ReporteFactory
+
+class Consultorio:
+    def sistemaConsultorio(self):
+        while True:
+            print("\n======= Consultorio CitaYA =======\n¡Bienvenido al lugar donde la salud es lo primero!\n")
+            print("1. Gestionar citas")
+            print("2. Generar un reporte")
+            print("3. Salir del sistema")
+
+            opcion = input("Indique la opción a realizar: ")
+
+            if opcion == "1":
+                break
+            elif opcion == "2":
+                self.obtenerReportes()
+            elif opcion == "3":
+                print("Saliendo...\n¡Gracias por creer en nosotros!")
+                break
+            else:
+                print("¡La opción no se encuentra disponible, intente nuevamente!")
+
+    def obtenerReportes(self):
+        reporte_factory = ReporteFactory()
+        while True:
+            reporte = reporte_factory.crear_reporte()
+            if reporte:
+                reporte.generar()
+            else:
+                break
+
+# Inicializar sistema
+consultorio = Consultorio()
+consultorio.sistemaConsultorio()
